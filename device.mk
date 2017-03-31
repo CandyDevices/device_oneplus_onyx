ada3e88a69f5fa39e394e4943f72e94a88d214ce
RJ Murdok <rjmurdok@linux.com>
2017-03-31 12:57:43 -0600

    onyx: opt in for AdAway

--- device.mk
+++ device.mk
@@ -178,6 +178,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+PRODUCT_PACKAGES += \
+    AdAway
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
